1c1bb23fb976d714a3cbbf6dadba5b49bff0d99d
Justin P. <justinpascua1459@gmail.com>
2026-02-26 11:45:51 -0800

    Implement pydantic models for transform layer

--- pipeline/transform/enums.py
+++ pipeline/transform/enums.py
@@ -4,16 +4,40 @@ class Chamber(enum.Enum):
     HR = "House of Representatives"
     S = "Senate"
 
+    @classmethod
+    def _missing_(cls, value):
+        missing_map = {
+            "H": cls.HR,
+            "HR": cls.HR,
+            "House": cls.HR,
+            "S": cls.S,
+        }
+        if value in missing_map:
+            return missing_map[value]
+        
+        return super()._missing_(value)
+
 class SponsorshipType(enum.Enum):
     SPONSOR = "sponsor"
     COSPONSOR = "cosponsor"
 
-class BillTypes(enum.Enum):
-    HR = "HR"
-    S = "S"
-    HJRES = "HJRES"
-    SJRES = "SJRES"
-    HCONRES = "HCONRES"
-    SCONRES = "SCONRES"
-    HRES = "HRES"
-    SRES = "SRES"
+class BillType(enum.Enum):
+    # need lower-case to allow values to be used in API calls
+    HR = "hr"
+    S = "s"
+    HJRES = "hjres"
+    SJRES = "sjres"
+    HCONRES = "hconres"
+    SCONRES = "sconres"
+    HRES = "hres"
+    SRES = "sres"
+
+    # used for case-insensitive instantiation
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            for member in cls:
+                if member.value == value.lower():
+                    return member
+    
+        return super()._missing_(value)
--- pipeline/transform/schemas.py
+++ pipeline/transform/schemas.py
@@ -1,1 +1,31 @@
 import pydantic
+from pydantic import BaseModel
+from typing import List, Optional
+from .enums import Chamber, BillType
+
+class RepresentativeClean(BaseModel):
+    bio_guide_id: str
+    name: str
+    party: Optional[str]
+    state: str
+    district: int | str | None = None
+    chamber: Chamber
+
+class BillClean(BaseModel):
+    congress_num: int
+    bill_type: BillType
+    bill_num: int
+
+    title: str
+    chamber: Chamber
+    policy_area: Optional[str] = None
+    summary: Optional[str] = None
+
+class BillMembershipClean(BaseModel):
+    # identifier for representative
+    bio_guide_id: str
+
+    # identifiers for bill
+    congress_num: int
+    bill_type: BillType
+    bill_num: int
