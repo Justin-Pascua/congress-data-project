a5add5abf3eec36ddf1a3bef498bbfc54a57bfb5
Justin P. <justinpascua1459@gmail.com>
2026-03-06 19:02:13 -0800

    Add exception handling to reset_all dag

--- airflow/dags/reset_all.py
+++ airflow/dags/reset_all.py
@@ -2,6 +2,7 @@ from airflow.sdk import dag, task, Param
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 from datetime import datetime, timedelta
+import logging
 
 from pipeline.tracking import utils
 
@@ -11,9 +12,11 @@ default_args = {
     "retry_delta": timedelta(seconds = 2)
 }
 
+logger = logging.getLogger(__name__)
+
 @dag(
     dag_id = "reset_all",
-    description = "USED INTERNALLY FOR DEV TESTING. Wipes all data from database and resets ledger file",
+    description = "USED INTERNALLY FOR DEV TESTING. Wipes all data from database and resets queue file",
     default_args = default_args,
     params={
        "congress_num": Param(default = 118, type = "integer", description = "Congress number to fetch"),
@@ -24,8 +27,14 @@ default_args = {
 )
 def reset_all():
     @task
-    def reset_ledger(**context):
-        utils._reset_ledger(context["params"]["congress_num"])
+    def reset_queue(**context):
+        congress_num = context["params"]["congress_num"]
+        try:
+            queue_df = utils.read_queue(congress_num)
+            queue_df = utils.reset_statuses(queue_df)
+            utils.commit_queue(congress_num, queue_df)
+        except:
+            logger.info("Queue file did not exist. No changes made")
 
     reset_db = SQLExecuteQueryOperator(
         task_id = "reset_db",
@@ -38,6 +47,6 @@ def reset_all():
         parameters={"congress_num": "{{ params.congress_num }}"}
     )
 
-    [reset_ledger(), reset_db]
+    [reset_queue(), reset_db]
 
 resetter_dag = reset_all()
