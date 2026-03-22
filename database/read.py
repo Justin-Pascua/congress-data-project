129ed41e1556a874870bbc048f302b073c908f36
Justin P. <justinpascua1459@gmail.com>
2026-03-22 16:50:17 -0700

    Add end_date to read_bills function in database/read.py. Reorganize and add to ml/utils/data.py

--- database/read.py
+++ database/read.py
@@ -30,18 +30,22 @@ def read_members(congress_num: int,
         result = db.scalars(stmt).all()
     return result
 
-def read_bills(congress_num: int,
+def read_bills(congress_num: int = None,
                chambers: List[Literal['HR', 'S']] = None,
-               start_date: datetime = None):
+               start_date: datetime = None,
+               end_date: datetime = None):
     """
     Pulls bills from bills table in local db.
     Args:
         congress_num: the number of the congress (e.g. 119) to filter by
         chambers: the chambers of congress to filter by
-        start_date: the datetime to filter by
+        start_date: the starting timestamp to filter by bill introduction date
+        end_date: the ending timestamp to filter by bill introduction date
     """
     with Session() as db:
-        stmt = select(Bill).where(Bill.congress_num == congress_num)
+        stmt = select(Bill)
+        if congress_num is not None:
+            stmt = stmt.where(Bill.congress_num == congress_num)
         if chambers is not None:
             bill_types = []
             if 'HR' in chambers:
@@ -50,7 +54,9 @@ def read_bills(congress_num: int,
                 bill_types.extend(['S', 'SJRES', 'SCONRES', 'SRES'])
             stmt = stmt.where(Bill.bill_type.in_(bill_types))
         if start_date is not None:
-            stmt = stmt.where(Bill.introduced_date >= start_date)
+            stmt = stmt.where(start_date <= Bill.introduced_date)
+        if end_date is not None:
+            stmt = stmt.where(Bill.introduced_date <= end_date)
         result = db.scalars(stmt).all()
     return result
 
--- ml/utils/data.py
+++ ml/utils/data.py
@@ -1,28 +1,28 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 import re
 from typing import List
 
-def strip_html_tags(text: str) -> str:
-    """
-    Remove HTML tags from a string using regex
-    Args:
-        text: a string possibly containing html tags
-    """
-    if not isinstance(text, str):
-        return text
-    
-    # Remove HTML tags
-    clean_text = re.sub(r'<[^>]+>', '', text)
-    
-    # Remove extra whitespace
-    clean_text = re.sub(r'\s+', ' ', clean_text).strip()
-    
-    return clean_text
+from database.models import Bill
 
-policy_area_mapping = {
+class BillDataset(Dataset):
+    def __init__(self, df: pd.DataFrame, feature_col: str | List[str], target_col: str | List[str]):
+        self.X = df[feature_col].reset_index(drop = True)
+        self.y = df[target_col].reset_index(drop = True)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, index):
+        # not applying tokenizer here. 
+        # delegate tokenization to DataLoader so that tokenizer can handle padding
+        x, y = self.X.iloc[index], self.y.iloc[index]
+        return x, y
+
+raw2simplified = {
     # Security & Defense
     "Armed Forces and National Security": "Security & Defense",
     "International Affairs": "Security & Defense",
@@ -80,24 +80,48 @@ def policy_area_simplifier(raw_policy_area: str) -> str:
         raw_polcy_area: a raw policy area string
     """
     try:
-        return policy_area_mapping[raw_policy_area]
+        return raw2simplified[raw_policy_area]
     except:
-        return raw_policy_area
-    
-class BillDataset(Dataset):
-    def __init__(self, df: pd.DataFrame, feature_col: str | List[str], target_col: str | List[str]):
-        self.X = df[feature_col].reset_index(drop = True)
-        self.y = df[target_col].reset_index(drop = True)
+        return "Other"
 
-    def __len__(self):
-        return len(self.y)
+possible_labels = list(set(raw2simplified.values())) + ["Other"]
 
-    def __getitem__(self, index):
-        # not applying tokenizer here. 
-        # delegate tokenization to DataLoader so that tokenizer can handle padding
-        x, y = self.X.iloc[index], self.y.iloc[index]
-        return x, y
+encoder = LabelEncoder()
+encoder.fit(possible_labels)
+
+def strip_html_tags(text: str) -> str:
+    """
+    Remove HTML tags from a string using regex
+    Args:
+        text: a string possibly containing html tags
+    """
+    if not isinstance(text, str):
+        return text
     
+    # Remove HTML tags
+    clean_text = re.sub(r'<[^>]+>', '', text)
+    
+    # Remove extra whitespace
+    clean_text = re.sub(r'\s+', ' ', clean_text).strip()
+    
+    return clean_text
+    
+def process_bills(bills: List[Bill]) -> dict:
+    """
+    Given a list of Bill objects from the database, returns a dataframe with cleaned summaries along with simplified and numerical labels.
+    Args:
+        bills: a list of `database.models.Bill` objects.
+    """
+    summaries = [strip_html_tags(bill.summary) for bill in bills]
+    labels = [policy_area_simplifier(bill.policy_area) for bill in bills]
+
+    df = pd.DataFrame(list(zip(summaries, labels)), columns = ['summary', 'label'])
+    df = df.dropna().reset_index(drop = True)
+    
+    df['numericalLabel'] = encoder.transform(df['label'])
+    
+    return df
+
 def make_collate_fn(tokenizer, max_length: int = None):
     """
     Factory function that creates collate_fn to be passed to a torch `DataLoader` object. 
